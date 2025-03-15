f7e7a02d171c6503e828c745f78c67693f60625a
DoniLite <yaomessan13@outlook.com>
2025-03-15 17:34:49 +0000

    feat: implement search keys features and add some test function

--- src/lib.rs
+++ src/lib.rs
@@ -8,7 +8,9 @@ pub struct Config {
 impl Config {
     pub fn new(args: &[String]) -> Result<Self, &'static str> {
         if args.len() < 3 {
-            return Err("You must provide arguments to the script example:  \n cargo run arg1 arg2");
+            return Err(
+                "You must provide arguments to the script example:  \n cargo run arg1 arg2",
+            );
         }
         Ok(Config {
             search_key: args[1].clone(),
@@ -19,10 +21,19 @@ impl Config {
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let file_path = path::Path::new(&config.file_path);
-    let content =
-        fs::read_to_string(file_path)?;
+    let content = fs::read_to_string(file_path)?;
     println!("The text content :\n{}", content);
     Ok(())
+}
+
+pub fn search<'a>(search_content: &str, content: &'a str) -> Vec<&'a str> {
+    let mut result: Vec<&str> = Vec::new();
+    for line in content.lines() {
+        if line.contains(search_content) {
+            result.push(line);
+        }
+    }
+    return result;
 }
 
 #[cfg(test)]
@@ -30,11 +41,23 @@ mod tests {
     use super::*;
 
     #[test]
-    fn one_result() {
+    fn test_runner() {
         let config = Config {
             search_key: "hello".to_string(),
             file_path: "poem.txt".to_string(),
         };
         assert!(run(config).is_ok());
+    }
+
+    #[test]
+    fn test_searcher() {
+        let search_key = "duct";
+        let content = "\
+    Rust:sécurité, rapidité, productivité.
+    Obtenez les trois en même temps.";
+        assert_eq!(
+            vec!["sécurité, rapidité, productivité."],
+            search(search_key, content)
+        );
     }
 }
