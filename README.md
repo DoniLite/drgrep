2f45a77addb3b802c1afbcf63f8012593ddd43c6
DoniLite <yaomessan13@outlook.com>
2025-03-17 19:34:14 +0000

    feat: add custom args parser and define build profiles

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # drgrep
 
-This project is my own implementation of the `grep` software used to search word in a text slice
+This project is my own implementation of the `grep` software used to search word in a text slice  
+This project can be used as executable or crate package
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,45 @@
-use std::{error::Error, fs, path};
+use std::{collections::HashMap, env, error::Error, fs, path};
 
+
+pub struct ArgParser {
+    pub args: HashMap<String, Option<String>>,
+}
+
+impl ArgParser {
+    pub fn new() -> Self {
+        let mut args = HashMap::new();
+        let mut iter = env::args().skip(1).peekable(); // Ignore le premier argument (nom du programme)
+
+        while let Some(arg) = iter.next() {
+            if arg.starts_with("--") {
+                let key = arg.trim_start_matches("--").to_string();
+                if let Some(value) = iter.peek() {
+                    if !value.starts_with("--") {
+                        args.insert(key, Some(iter.next().unwrap()));
+                    } else {
+                        args.insert(key, None);
+                    }
+                } else {
+                    args.insert(key, None);
+                }
+            } else if arg.starts_with("-") {
+                let key = arg.trim_start_matches("-").to_string();
+                args.insert(key, None);
+            }
+        }
+
+        Self { args }
+    }
+
+    pub fn get(&self, key: &str) -> Option<&Option<String>> {
+        self.args.get(key)
+    }
+
+    pub fn has(&self, key: &str) -> bool {
+        self.args.contains_key(key)
+    }
+}
+/// The config struct
 pub struct Config {
     pub search_key: String,
     pub file_path: String,
