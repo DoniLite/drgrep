f891e25550c2509ec444c7675da53fd0805a681f
DoniLite <yaomessan13@outlook.com>
2025-04-09 19:24:16 +0000

    feat: add module parser and color printer - add the parser module for more parse args - add the color module for creating beautiful coloration on the shell - add some workflow for publish build and docs - refactor the architecture

---
+++ src/color/config.rs
@@ -0,0 +1,31 @@
+// src/color/config.rs
+
+pub struct Color;
+
+impl Color {
+    pub const RESET: &'static str = "\x1b[0m";
+
+    // Text colors
+    pub const BLACK: &'static str = "\x1b[30m";
+    pub const RED: &'static str = "\x1b[31m";
+    pub const GREEN: &'static str = "\x1b[32m";
+    pub const YELLOW: &'static str = "\x1b[33m";
+    pub const BLUE: &'static str = "\x1b[34m";
+    pub const MAGENTA: &'static str = "\x1b[35m";
+    pub const CYAN: &'static str = "\x1b[36m";
+    pub const WHITE: &'static str = "\x1b[37m";
+
+    // Bright variants
+    pub const BRIGHT_BLACK: &'static str = "\x1b[90m";
+    pub const BRIGHT_RED: &'static str = "\x1b[91m";
+    pub const BRIGHT_GREEN: &'static str = "\x1b[92m";
+    pub const BRIGHT_YELLOW: &'static str = "\x1b[93m";
+    pub const BRIGHT_BLUE: &'static str = "\x1b[94m";
+    pub const BRIGHT_MAGENTA: &'static str = "\x1b[95m";
+    pub const BRIGHT_CYAN: &'static str = "\x1b[96m";
+    pub const BRIGHT_WHITE: &'static str = "\x1b[97m";
+
+    // Styles
+    pub const BOLD: &'static str = "\x1b[1m";
+    pub const UNDERLINE: &'static str = "\x1b[4m";
+}
