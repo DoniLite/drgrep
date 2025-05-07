af283eebc8a3ca32e6db8920b2263668a6664244
DoniLite <DoniLite@users.noreply.github.com>
2025-05-07 17:05:22 +0000

    Deploying to gh-pages from @ DoniLite/drgrep@2cc4a7eda143f68f4ef255e2b935157438a6b8b7 🚀

--- src-files.js
+++ src-files.js
@@ -1,3 +1,3 @@
-var srcIndex = new Map(JSON.parse('[["drgrep",["",[["args",[],["mod.rs","parser.rs"]],["color",[],["config.rs","mod.rs","printer.rs"]],["regex",[],["mod.rs","pattern.rs"]]],["lib.rs"]]]]'));
+var srcIndex = new Map(JSON.parse('[["drgrep",["",[["args",[],["mod.rs","parser.rs"]],["color",[],["config.rs","mod.rs","printer.rs"]],["regex",[],["mod.rs","pattern.rs"]]],["glob.rs","lib.rs","temp_dir.rs"]]]]'));
 createSrcSidebar();
-//{"start":36,"fragment_lengths":[149]}
+//{"start":36,"fragment_lengths":[173]}
