ec32e7e54f7c76a93676b2f00fcdf48104225d72
DoniLite <DoniLite@users.noreply.github.com>
2025-05-20 20:57:51 +0000

    Deploying to gh-pages from @ DoniLite/drgrep@3a2e9bb44c37439afe82997036262cc30e02d5a7 🚀

--- drgrep/utilities/sidebar-items.js
+++ drgrep/utilities/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":["can_read_to_utf8","visit_dirs"],"struct":["GitIgnoreFiles"]};
+window.SIDEBAR_ITEMS = {"fn":["can_read_to_utf8","read_stdin","visit_dirs"],"struct":["GitIgnoreFiles"]};
--- trait.impl/core/default/trait.Default.js
+++ trait.impl/core/default/trait.Default.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["drgrep",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.86.0/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"drgrep/args/parser/struct.ArgParser.html\" title=\"struct drgrep::args::parser::ArgParser\">ArgParser</a>"]]]]);
+    var implementors = Object.fromEntries([["drgrep",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.87.0/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"drgrep/args/parser/struct.ArgParser.html\" title=\"struct drgrep::args::parser::ArgParser\">ArgParser</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
